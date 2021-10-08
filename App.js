1e9a9fce6ab513cc85e85819c57f7179cc61fc30
Lee Naeun <nanni4287@gmail.com>
2021-10-08 15:47:44 +0900

    스트라이크 볼 아웃 조건문 걸기

--- App.js
+++ App.js
@@ -1,38 +1,82 @@
 import * as readLine from 'readline-sync';
 
 function main() {
-  console.log('중복없이 1~9 중 숫자 세 가지를 고르세요.');
-  const input = readLine.question();
-  const spl = input.split(' ');
-  checking(spl);
+  while (true) {
+    console.log('중복없이 1~9 중 숫자 세 가지를 고르세요.');
+    const input = readLine.question();
+    console.log('input', input);
+    const spl = input.split(' ');
+    checking(spl);
+  }
 }
 
 function checking(arr) {
   // 숫자 중복이 있는지 없는지 확인, 1~9 중 숫자 고르기, 숫자는 3개만 고르기
   const newArr = arr.map((el) => Number(el));
+  const baseBallQuiz = randomNum();
+  console.log(baseBallQuiz);
+  console.log();
+
+  // newArr이랑 baseBallQuiz랑 비교해줘야함
 
   for (let i = 0; i < newArr.length; i++) {
-    if (newArr[i] >= 10 || newArr[i] < 1) {
-      console.log('1 ~ 9 중에서 숫자를 골라주세요.');
-      break;
-    } else {
+    if (newArr[i] < 10 && newArr[i] >= 1) {
+      // 1 ~ 9까지
       const isDupli = new Set(newArr).size;
 
       if (newArr.length !== isDupli || newArr.length !== 3) {
         console.log(
           '숫자 중 중복이 있거나 숫자 세 가지를 선택하지 않으셨습니다.'
         );
-        break;
-      } else {
-        console.log(randomNum());
+        console.log();
         break;
       }
+    } else {
+      // 1 ~ 9 사이 숫자가 아닐때
+      console.log('1 ~ 9 중에서 숫자를 골라주세요.');
+      console.log();
+      break;
     }
+  }
+
+  let ballCount = 0;
+  let strikeCount = 0;
+
+  for (let j = 0; j < newArr.length; j++) {
+    if (newArr[j] < 10 && newArr[j] >= 1) {
+      const howManyBall = baseBallQuiz.includes(newArr[j]);
+
+      // baseBallQuiz가 가지고 있는 배열 요소를 newArr[j]가 가지고 있고, baseBallQuiz의 j번쨰와 newArr[j]번째의 숫자가 같은지 확인
+
+      if (howManyBall && newArr[j] === baseBallQuiz[j]) {
+        strikeCount += 1;
+      } else if (howManyBall) {
+        ballCount += 1;
+      }
+
+      // if (howManyBall) {
+      //   ballCount += 1;
+      //   console.log(`${ballCount} ball`);
+      // }
+    } else {
+      break;
+    }
+  } // ball checking
+
+  if (strikeCount === 0 && ballCount === 0) {
+    console.log('Out!');
+  } else if (strikeCount !== 0 && ballCount === 0) {
+    console.log(`${strikeCount} Strik!`);
+  } else if (strikeCount === 0 && ballCount !== 0) {
+    console.log(`${ballCount} Ball!`);
+  } else {
+    console.log(`${strikeCount} Strik!`);
+    console.log(`${ballCount} Ball!`);
   }
 }
 
 function randomNum() {
-  // f랜덤으로 숫자가 중복되지 않게 뽑아내기
+  // 랜덤으로 숫자가 중복되지 않게 뽑아내기
   let randomArr = [];
 
   while (randomArr.length !== 3) {
