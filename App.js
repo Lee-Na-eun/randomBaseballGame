a1aa0b931b62d4731d27f6db0c4dd80d2495a4b4
Lee Naeun <nanni4287@gmail.com>
2021-10-11 18:55:11 +0900

    드디어 완성!

--- App.js
+++ App.js
@@ -2,21 +2,24 @@ import * as readLine from 'readline-sync';
 
 function main() {
   let answerCount = 0;
+  let randomQuiz = randomNum();
   while (true) {
     console.log('중복없이 1~9 중 숫자 세 가지를 고르세요.');
     const input = readLine.question();
     // console.log('input', input);
     const numInput = input.split(' ').map((el) => Number(el));
-    const random = `${randomNum()}`;
+    const random = `${randomQuiz}`;
 
     if (String(numInput) !== random) {
-      checking(numInput, randomNum());
+      checking(numInput, randomQuiz);
       answerCount += 1;
       console.log(`지금까지 ${answerCount}번 하셨습니다.`);
+      console.log();
     } else {
       answerCount += 1;
       console.log(`${answerCount}번만에 맞추셨습니다.`);
       console.log('끌게요!');
+      console.log();
       break;
     }
   }
@@ -25,10 +28,9 @@ function main() {
 function checking(arr, randomArr) {
   // 숫자 중복이 있는지 없는지 확인, 1~9 중 숫자 고르기, 숫자는 3개만 고르기
   const baseBallQuiz = randomArr;
-  console.log('baseBall', baseBallQuiz);
   console.log();
 
-  // newArr이랑 baseBallQuiz랑 비교해줘야함
+  // randomArr이랑 baseBallQuiz랑 비교해줘야함
 
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] < 10 && arr[i] >= 1) {
@@ -105,9 +107,9 @@ function randomNum() {
       randomArr.push(oneNine);
     }
   }
-  // return randomArr;
+  return randomArr;
   // input에 입력하면 랜덤값이 계속 바뀐다. 이 문제는 어떻게...?
-  return [1, 2, 3];
+  // return [1, 2, 3];
 }
 
 // 숫자 중복 X -> (o)
